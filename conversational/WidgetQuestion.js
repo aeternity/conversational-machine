bbf11c8310076d9f0e33327dcfa9efcdd86a7cf3
Philipp Klein <philipp@apeunit.com>
2017-08-23 14:36:52 +0200

    WidgetQuestion added

---
+++ conversational/WidgetQuestion.js
@@ -0,0 +1,14 @@
+let Question = require('Question');
+class WidgetQuestion extends Question {
+	constructor(questionText, widget, settings ) {
+		super(questionText, settings)
+		this.widget = widget;
+	}
+	get widget() {
+		return this._widget;
+	}
+	set widget(widget) {
+		this._widget = widget;
+	}
+}
+module.exports = WidgetQuestion;
